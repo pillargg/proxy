84037eb07811c82a7375b2f50f8534afbf72740b
Teddy Byron <ted@tedbyron.com>
2021-11-03 13:54:14 -0400

    add deps

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
-#![forbid(unsafe_code)]
-#![doc = include_str!("../README.md")]
+use lambda_http::handler;
+use lambda_http::lambda_runtime::{Context, Error};
+use lambda_http::{IntoResponse, Request, RequestExt, Response};
 
-fn main() {
-    println!("proxy");
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(handler(func)).await?;
+
+    Ok(())
+}
+
+async fn func(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    Ok(match event.query_string_parameters().get("first_name") {
+        Some(first_name) => format!("Hello, {}!", first_name).into_response(),
+        _ => Response::builder()
+            .status(400)
+            .body("Empty first name".into())
+            .expect("failed to render response"),
+    })
 }
