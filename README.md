1b747e9f8bc1183b1064b8852235a24aff2e51ae
Teddy Byron <ted@tedbyron.com>
2021-11-08 01:21:55 -0500

    almost there

--- README.md
+++ README.md
@@ -3,35 +3,56 @@
   <p><strong>HTTP relay for AWS Lambda.</strong></p>
 </div>
 
-## Build - x86_64
+## Build
 
-- Building on x86_64 GNU/Linux (`glibc`)
+The toolchain target `x86_64-unknown-linux-gnu` is required for AWS x86_64 processors.
 
-  ```sh
+```sh
+rustup target add x86_64-unknown-linux-gnu
+```
+
+### Building on `x86_64-*-linux-gnu`
+
+```sh
+cargo build --release --target x86_64-unknown-linux-gnu
+```
+
+### Building with [`cross`](https://github.com/rust-embedded/cross)
+
+  1. Install `cross`
+
+     ```sh
+     cargo install cross --version 0.2.1
+     ```
+
+  2. Build (uses [`rustembedded/cross:x86_64-unknown-linux-gnu-0.2.1`](https://hub.docker.com/layers/rustembedded/cross/x86_64-unknown-linux-gnu-0.2.1/images/sha256-9f368a726a8ba08559451cd64160f7d2b47f6180ad024a46e31d29cc85dd81ff) Docker image with `libssl-dev` installed)
+
+     ```sh
+     cross build --release --target x86_64-unknown-linux-gnu
+     ```
+
+### Building with Docker
+
+```sh
+docker run \
+  --rm \
+  --platform linux/amd64 \
+  --user "$(id -u):$(id -g)" \
+  --volume "${PWD}:/usr/src/relay" \
+  --workdir '/usr/src/relay' \
+  rust:latest \
   cargo build --release --target x86_64-unknown-linux-gnu
-  ```
-
-- Building on another OS with Docker
-
-  - Install [`cross`](https://github.com/rust-embedded/cross)
-
-    ```sh
-    cargo install cross --version 0.2.1
-    ```
-
-  - Build (uses [`rustembedded/cross:x86_64-unknown-linux-gnu-0.2.1`](https://hub.docker.com/layers/rustembedded/cross/x86_64-unknown-linux-gnu-0.2.1/images/sha256-9f368a726a8ba08559451cd64160f7d2b47f6180ad024a46e31d29cc85dd81ff) Docker image with `libssl-dev` installed)
-
-    ```sh
-    cross build --release --target x86_64-unknown-linux-gnu
-    ```
+```
 
 ## Dependency explanation
+
+- [`bytes`](https://lib.rs/crates/bytes) Bytes container used by `reqwest::Response::bytes`
 
 - [`lambda_http`](https://lib.rs/crates/lambda_http) Library for AWS API Gateway proxy event focused AWS Lambda functions
 
   - [`lambda_runtime`](https://lib.rs/crates/lambda_runtime) AWS Lambda runtime
 
-- [`reqwest`](https://lib.rs/crates/reqwest) Async HTTP Client with connection pooling
+- [`reqwest`](https://lib.rs/crates/reqwest) Async HTTP Client
 
 - [`tokio`](https://lib.rs/crates/tokio) Async runtime
 
--- src/main.rs
+++ src/main.rs
@@ -2,103 +2,156 @@
 #![forbid(unsafe_code)]
 #![doc = include_str!("../README.md")]
 
+use std::env;
+use std::time::Duration;
+
 use lambda_http::lambda_runtime::{self, Context, Error};
 use lambda_http::{handler, IntoResponse, Request, RequestExt, Response};
 use reqwest::Client;
 
+use util::{IntoLambdaBody, IntoReqwestBody};
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    std::env::set_var("RUST_BACKTRACE", "1"); // TODO: move to .env
-    std::env::set_var("TARGET", "rs.fullstory.com"); // TODO: move to .env
+    // TODO: move to `.env`.
+    env::set_var("RUST_BACKTRACE", "1");
+    env::set_var("TARGET", "https://rs.fullstory.com"); // Request target with scheme and host.
+    env::set_var("TIMEOUT", "10"); // Request timeout, in seconds.
 
+    // Start the Lambda runtime and begin polling for events.
     lambda_runtime::run(handler(entry)).await?;
+
+    // Ok if no error is propogated from the runtime.
     Ok(())
 }
 
+/// Takes an HTTP request and a Lambda function execution context, and transforms the request before
+/// sending it to the domain specified by the `$TARGET` environment variable. Returns the HTTP
+/// response from `$TARGET`.
 #[allow(clippy::unused_async)]
 async fn entry(req: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    // Cache the query parameters.
-    let query_params = req.query_string_parameters();
-    // Get the source address.
-    let source_addr = get_source_addr(&req);
-    // Separate the `Request` into head and body.
-    let (mut parts, body) = req.into_parts();
-
-    // Convert `lambda_http::Request` into a `hyper::Request`.
-    let body = match body {
-        Body::Empty => hyper::Body::empty(),
-        Body::Text(t) => hyper::Body::from(t.into_bytes()),
-        Body::Binary(b) => hyper::Body::from(b),
-    };
-    let mut uri = format!(
-        "https://{}{}",
-        std::env::var("TARGET").expect("Missing environment variable $TARGET"),
-        parts.uri.path()
+    // Get environment variables
+    let target = env::var("TARGET").expect("Missing environment variable $TARGET");
+    let timeout = Duration::from_secs(
+        env::var("TIMEOUT")
+            .expect("Missing environment variable $TIMEOUT")
+            .parse::<u64>()
+            .expect("Environment variable $TIMEOUT not a valid u64"),
     );
 
-    // URL encode the query parameters again and place them back into the URI because
-    // `lambda_runtime` automatically parses query params AND removes those query parameters from
-    // the original URI.
-    if !query_params.is_empty() {
-        append_querystring_from_map(&mut uri, query_params.iter());
+    // Avoid a drop while borrowing later by getting query params and then converting a reference to
+    // it into a Vec.
+    let query_params = req.query_string_parameters();
+    let query_params: Vec<(&str, &str)> = query_params.iter().collect();
+    // Move request into parts and body.
+    let (parts, body) = req.into_parts();
+    // Parse the url and query parameters for the new request.
+    let url = reqwest::Url::parse_with_params(&target, &query_params)?;
+
+    // Creating and send the request, saving the response to return from the Lambda.
+    let reqwest_response = Client::builder()
+        .build()?
+        .request(parts.method, url)
+        .version(parts.version)
+        .body(body.into_reqwest_body())
+        .timeout(timeout)
+        .send()
+        .await?;
+
+    // Create a new response to return from the Lambda function.
+    let mut lambda_response = Response::builder()
+        .status(reqwest_response.status())
+        .version(reqwest_response.version());
+
+    // Append headers to the response.
+    match lambda_response.headers_mut() {
+        Some(headers) => {
+            todo!()
+        }
+        _ => (), // FIX: this match arm will only happen if there is a builder error.
     }
 
-    // Convert the `String` into a `hyper::Uri`.
-    parts.uri = match uri.parse::<hyper::Uri>() {
-        Ok(uri) => uri,
-        Err(e) => panic!("Failed to build uri: {:?}", e),
-    };
+    // Convert the reqwest response body to a `lambda_http::Body`.
+    let lambda_response =
+        lambda_response.body(reqwest_response.bytes().await?.into_lambda_body())?;
 
-    // Rebuild the request as a `hyper::Request` using its modified parts.
-    let req = hyper::Request::from_parts(parts, body);
-
-    // TODO: relay request
-    // let response = lambda_http::http::request::Builder::new()
-    //     .method("POST")
-    //     .body(())
-    //     .unwrap();
-
-    // Convert the `hyper::Request` into a `lambda_http::Request`.
-    // let (parts, body) = response.into_parts();
-    let body_bytes = hyper::body::to_bytes(body).await?;
-    let body = String::from_utf8(body_bytes.to_vec()).unwrap();
-    Ok(Response::from_parts(parts, Body::from(body)))
-    // Ok(match query_params.get("first_name") {
-    //     Some(first_name) => format!("Hello, {}!", first_name).into_response(),
-    //     _ => Response::builder()
-    //         .status(400)
-    //         .body("Empty first name".into())
-    //         .expect("Failed to render response"),
-    // })
+    Ok(lambda_response)
 }
 
-fn get_source_addr(req: &Request) -> SocketAddr {
-    let source_ip: String = match req.request_context() {
-        RequestContext::ApiGateway(ApiGatewayRequestContext { identity, .. }) => identity.source_ip,
-        RequestContext::ApiGatewayV2(ApiGatewayV2RequestContext {
-            http: Http { source_ip, .. },
-            ..
-        }) => source_ip,
-        RequestContext::Alb(_) => Ipv4Addr::UNSPECIFIED.to_string(),
-    };
+mod util {
+    use bytes::Bytes;
 
-    SocketAddr::new(
-        source_ip.parse().unwrap(),
-        std::env::var("PORT")
-            .expect("Missing environment variable $PORT")
-            .parse::<u16>()
-            .expect("$PORT environment variable not a valid port"),
-    )
-}
-
-fn append_querystring_from_map<'a, I>(uri: &mut String, from_query_params: I)
-where
-    I: Iterator<Item = (&'a str, &'a str)>,
-{
-    uri.push('?');
-    let mut serializer = url::form_urlencoded::Serializer::new(String::new());
-    for (key, value) in from_query_params.into_iter() {
-        serializer.append_pair(key, value);
+    pub trait IntoReqwestBody {
+        fn into_reqwest_body(self) -> reqwest::Body;
     }
-    uri.push_str(serializer.finish().as_str())
+
+    impl IntoReqwestBody for lambda_http::Body {
+        /// Converts the `lambda_http::Body` enum into a `reqwest::Body` struct.
+        fn into_reqwest_body(self) -> reqwest::Body {
+            match self {
+                lambda_http::Body::Empty => reqwest::Body::from(""),
+                lambda_http::Body::Text(t) => reqwest::Body::from(t),
+                lambda_http::Body::Binary(b) => reqwest::Body::from(b),
+            }
+        }
+    }
+
+    pub trait IntoLambdaBody {
+        fn into_lambda_body(self) -> lambda_http::Body;
+    }
+
+    impl IntoLambdaBody for Bytes {
+        /// Zero-clone conversion of `Bytes` into `lambda_http::Body`.
+        fn into_lambda_body(self) -> lambda_http::Body {
+            if self.is_empty() {
+                lambda_http::Body::Empty
+            } else {
+                // Copy the bytes into a Vec of bytes.
+                lambda_http::Body::Binary(self.to_vec())
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use lambda_http::http::Method;
+    use lambda_http::request::{
+        AlbRequestContext, ApiGatewayRequestContext, ApiGatewayV2RequestContext, Http,
+    };
+
+    use super::*;
+
+    // #[tokio::test]
+    // async fn return_200_and_response() {
+    //     let mut request = lambda_http::Request::new(lambda_http::Body::Empty);
+    //     *request.uri_mut() = "/".parse().unwrap();
+    //     request
+    //         .extensions_mut()
+    //         .insert::<RequestContext>(RequestContext::ApiGatewayV2(ApiGatewayV2RequestContext {
+    //             route_key: None,
+    //             account_id: None,
+    //             stage: None,
+    //             request_id: None,
+    //             authorizer: None,
+    //             apiid: None,
+    //             domain_name: None,
+    //             domain_prefix: None,
+    //             time: None,
+    //             time_epoch: 0,
+    //             http: Http {
+    //                 method: Method::GET,
+    //                 path: None,
+    //                 protocol: None,
+    //                 source_ip: Some("127.0.0.1".to_string()),
+    //                 user_agent: None,
+    //             },
+    //         }));
+    //     let response = entrypoint(request, Context::default())
+    //         .await
+    //         .unwrap()
+    //         .into_response();
+    //     assert_eq!(StatusCode::OK, response.status());
+    //     assert!(!response.body().is_empty())
+    // }
 }
